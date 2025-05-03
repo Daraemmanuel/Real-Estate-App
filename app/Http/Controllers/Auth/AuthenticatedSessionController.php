ada21cced3009fb4600367ac02de4857777d7485
Dara Akinyode || Kodemmy <Immanueldarasimi@gmail.com>
2025-05-03 23:09:45 +0100

    3.05.25 Lat Update For Today

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -44,7 +44,8 @@ class AuthenticatedSessionController extends Controller
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): RedirectResponse
+    
+     public function destroy(Request $request): RedirectResponse
     {
         Auth::guard('web')->logout();
 
